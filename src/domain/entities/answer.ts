ead678992572db968bb74229a34e12226b7523e6
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-01-19 15:27:55 -0300

    Fundamentos de DDD - Mapeando relacionamentos

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,10 +1,20 @@
+interface AnswerProps {
+  content: string
+  authorId: string
+  questionId: string
+}
+
 export class Answer {
   public id: string
   public content: string
+  public authorId: string
+  public questionId: string
 
-  constructor(content: string, id?: string) {
+  constructor({ authorId, content, questionId }: AnswerProps, id?: string) {
     // Se tiver id, ele recebe o id, se não tiver, ele cria um id aleatório
     this.id = id ?? crypto.randomUUID()
     this.content = content
+    this.authorId = authorId
+    this.questionId = questionId
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,19 @@
+interface QuestionProps {
+  title: string
+  content: string
+  authorId: string
+}
+
 export class Question {
   public id: string
   public title: string
   public content: string
+  public authorId: string
 
-  constructor(title: string, content: string, id?: string) {
+  constructor({ authorId, title, content }: QuestionProps, id?: string) {
     // Se tiver id, ele recebe o id, se não tiver, ele cria um id aleatório
     this.id = id ?? crypto.randomUUID()
+    this.authorId = authorId
     this.title = title
     this.content = content
   }
